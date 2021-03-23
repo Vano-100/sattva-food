cc7d1b1cd29220b9f6c7a1dec17cfd646247e3da
Ivan <vano-100@mail.ru>
2021-03-23 21:20:55 +0700

    initial commit

---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike slim Shirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adudas slim Shirt',
+            category: 'Shirts',
+            image:'./p1.jpg',
+            price: 100,
+            brand: 'Adudas',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Purnima',
+            category: 'Shirts',
+            image:'./p1.jpg',
+            price: 80,
+            brand: 'Purnima',
+            rating: 3.5,
+            numReviews: 5,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike slim Shirt',
+            category: 'Shirts',
+            image:'./p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 3,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Nike slim Shirt',
+            category: 'Shirts',
+            image:'./p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 4,
+            description: 'high quality product'
+        }
+    ]
+};
+export default data;
